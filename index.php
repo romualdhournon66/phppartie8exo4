3a5e43ff6a825d60e3ec44e261243079da3fa7b8
romualdhournon66 <k2001@cegetel.net>
2018-12-17 21:25:54 +0100

    mes php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="FR">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <title>PHP Partie 8 exercice 4</title>
+    </head>
+    <body>
+        <div class="container" id="page-top">
+            <div class="row">
+                <div class="col-xl-12">
+                    <h1 align="center">PHP Partie 8 exercice 4</h1>
+                    <h3 align="center">Exercice 4</h3>
+                    <p align="center">Faire une page qui va récupérer les informations du cookie créé à l'exercice 3 et qui les affiches.</p>
+                </div>
+            </div>
+            <form method="POST" action="user.php">
+                <div align="center"><input type="text" placeholder="Votre login" name="login" /></div>
+                <div align="center"><input type="password" placeholder="Votre password" name="password" /></div>
+                <div align="center"><input type="submit" value="Valider" /></div>
+            </form>
+        </div>
+    </body>
+</html>
